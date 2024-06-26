ed24ccc952c96f8be63e63fd49ff6b0ca49b4d0d
devcastro7712 <devcastro7712@gmail.com>
2024-06-26 20:30:36 -0300

    using modules
    complementary exercises

--- ex019.py
+++ ex019.py
@@ -1,2 +1,9 @@
 #Um professor quer sortear um dos alunos para apagar o quadro. Faça um programa que ajude ele lendo o nome deles e escrevendo
 #um nome na tela
+import random
+
+lista = ("lucas", "sérgio", "andré", "bruno")
+
+
+print("o aluno sorteado foi {}".format(random.choice(lista)))
+
--- ex020.py
+++ ex020.py
@@ -1,2 +1,6 @@
-#O mesmo professor do desafio anterior quer soetear a ordem de apresenteção do trabalho dos alunos.
+#O mesmo professor do desafio anterior quer sortear a ordem de apresenteção do trabalho dos alunos.
 #Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada
+import random
+lista = ["lucas", "sérgio", "andré", "bruno"]
+random.shuffle(lista)
+print("a nova ordem de será a seguinte: {}".format(lista))
